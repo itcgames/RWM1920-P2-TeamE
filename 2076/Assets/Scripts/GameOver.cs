5ca94974e33668661e256520e4cb00d00d2c6d5d
Aoife <c00218374@itcarlow.ie>
2019-11-26 11:30:19 +0000

    Particles

---
+++ 2076/Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    bool gameOver = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            gameOver = true;
+        }
+    }
+}
---
+++ 2076/Assets/Scripts/Particles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+    public ParticleSystem dust;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.magnitude > 0)
+        {
+            dust.transform.position = new Vector3(transform.position.x + 0.2f, transform.position.y - 0.3f, transform.position.z);
+            dust.Play();
+            transform.hasChanged = false;
+        }
+    }
+}
