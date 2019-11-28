dcd3c7b9473730f9ead06911813d461b6602bc2b
Alex OToole <tooler1999@gmail.com>
2019-11-28 09:57:51 +0000

    added animations to the game, Also edited HUD buttons

--- 2076/Assets/Scripts/Accelerator/Acceleration.cs
+++ 2076/Assets/Scripts/Accelerator/Acceleration.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Acceleration : MonoBehaviour
 {
     public GameObject player;
+    private Animator anim;
 
     float angle;
     float speed;
@@ -16,7 +17,8 @@ public class Acceleration : MonoBehaviour
     void Start()
     {
         angle = transform.rotation.eulerAngles.z - 90;
-        speed = 2.0f;
+        speed = 0.5f;
+        anim = GetComponent<Animator>();
         Debug.Log(angle);
     }
 
@@ -25,6 +27,7 @@ public class Acceleration : MonoBehaviour
     {
         if(accelerate == true)
         {
+            anim.SetBool("Fire", true);
             Vector2 newSpeed = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle) * speed, Mathf.Sin(Mathf.Deg2Rad * angle) * speed);
 
             player.GetComponent<Rigidbody2D>().velocity += newSpeed;
@@ -36,7 +39,6 @@ public class Acceleration : MonoBehaviour
 
             player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, player.GetComponent<Rigidbody2D>().velocity.y + newSpeed.y);
         }
-        Debug.Log(player.GetComponent<Rigidbody2D>().velocity);
     }
 
     public void correctVelocity()
