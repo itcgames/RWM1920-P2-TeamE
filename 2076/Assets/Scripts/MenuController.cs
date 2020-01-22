5d95a2147740d6e611c78e4770d35db2e2ff0e99
AlexOToole <tooler1999@gmail.com>
2020-01-22 11:45:38 +0000

    added level select screen

--- 2076/Assets/Scripts/MenuController.cs
+++ 2076/Assets/Scripts/MenuController.cs
@@ -8,11 +8,33 @@ public class MenuController : MonoBehaviour
     public GameObject menu;
     public GameObject optionsMenu;
     public GameObject helpMenu;
-    public void playGame()
+    public GameObject levelSelect;
+
+    public void levelOne()
     {
         SceneManager.LoadScene("Level 1");
     }
 
+    public void levelTwo()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void levelThree()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void custom()
+    {
+        SceneManager.LoadScene("Custom");
+    }
+
+    public void selectLevel()
+    {
+        menu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
     public void options()
     {
         menu.SetActive(false);
@@ -30,6 +52,7 @@ public class MenuController : MonoBehaviour
         menu.SetActive(true);
         optionsMenu.SetActive(false);
         helpMenu.SetActive(false);
+        levelSelect.SetActive(false);
     }
 
     public void exitGame()
